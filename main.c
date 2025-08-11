1df7d08813aa2bf33d71a39654c809beb0608354
Matrix030 <rishigharat@gmail.com>
2025-08-11 19:30:02 -0400

    added cd

--- main.c
+++ main.c
@@ -8,6 +8,40 @@
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
+
+//built in shell commands
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int (builtin_func[]) (char **) = {
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
+};
+
+int lsh_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+	if(args[1] == NULL) {
+		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	} else {
+		if(chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+	return 1;
+}
 char *lsh_read_line(void)
 {
 	int bufsize = LSH_RL_BUFSIZE;
